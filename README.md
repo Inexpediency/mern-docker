77efadac90ce7b4571aea64cfbf8bae7419b17d0
ythosa <vasus714@yandex.ru>
2020-07-08 20:32:01 +0300

    Add environment variables

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # dockerizing
 
-<br>
-
 ### Description
 * Docker Usage Example
 * Template for next projects with docker & docker-compose
 
 
 ### Available scripts:
-* docker-compose build - build application
-* docker-compose up - run application
+* `docker-compose build` - build application
+* `docker-compose up` - run application
+* `docker-compose up --build` - run and build? application
--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+
 app.get('/test', (req, res) => {
-    res.send('Our api server is working correctly')
+    res.send('Our api server is working correctly :3')
 })
 
-app.listen(3000, () => {
-    console.log('Started api service')
+app.listen(PORT, () => {
+    console.log(`Started api service on
+        port: ${PORT}
+        host: ${HOST}
+    `)
 })
