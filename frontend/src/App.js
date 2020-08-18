3faa7016cc3669f5f5ec641c002e33271a3bf51d
ythosa <vasus714@yandex.ru>
2020-08-18 23:54:14 +0300

    Proxy frontend requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+    const makeAPIRequest = () => {
+        axios.get('/auth/test').then(res => {
+            console.log(`Test API server result: ${res.data}`)
+        })
+
+        axios.get('/api/test').then(res => {
+            console.log(`Test AUTH server result: ${res.data}`)
+        })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Lol
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload. Lol
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+        <button onClick={makeAPIRequest}>Make API Request</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
