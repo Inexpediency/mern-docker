2bd41bc7adc8802fcf442edb9d8e7c87a5cf861b
ythosa <vasus714@yandex.ru>
2020-08-19 14:07:59 +0300

    Add basic struct of mailer service

---
+++ mailer/src/cmd/apiserver/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+}
