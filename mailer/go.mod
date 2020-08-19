367d1500dde3f7d473e54f2ea82514db90376b99
ythosa <vasus714@yandex.ru>
2020-08-19 15:05:42 +0300

    Add test sending handler

--- mailer/go.mod
+++ mailer/go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.4
 	github.com/sirupsen/logrus v1.6.0
 )
--- mailer/src/internal/app/apiserver/server.go
+++ mailer/src/internal/app/apiserver/server.go
@@ -1,10 +1,14 @@
 package apiserver
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/google/uuid"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
@@ -28,7 +32,19 @@ func newServer() *server {
 }
 
 func (s *server) configurateRouter() {
+	s.router.Use(s.setRequestID)
 	s.router.Use(s.logRequest)
+	s.router.Use(handlers.CORS(handlers.AllowedOrigins([]string{"*"})))
+
+	s.router.HandleFunc("/send_test", s.handleTestSending()).Methods("POST")
+}
+
+func (s *server) setRequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New().String()
+		w.Header().Set("X-Request-ID", id)
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), ctxKeyRequestID, id)))
+	})
 }
 
 func (s *server) logRequest(next http.Handler) http.Handler {
@@ -50,4 +66,34 @@ func (s *server) logRequest(next http.Handler) http.Handler {
 			time.Now().Sub(start),
 		))
 	})
+}
+
+func (s *server) handleTestSending() http.HandlerFunc {
+	type request struct {
+		Email string `json:"email"`
+		Text  string `json:"text"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, map[string]string{
+			"ok": fmt.Sprintf("message sent to %s", req.Email),
+		})
+	}
+}
+
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
+	}
 }
